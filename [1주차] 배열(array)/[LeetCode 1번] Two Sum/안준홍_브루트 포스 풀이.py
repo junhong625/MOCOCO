5679c48d6439211bca79a5935deeecc5530a2cd2
Junhong Ahn <junhong625@naver.com>
2022-07-25 22:23:00 +0900

    2022/07/25 풀이

--- [1주차] 배열(array)/[LeetCode 1번] Two Sum/안준홍_브루트 포스 풀이.py
+++ [1주차] 배열(array)/[LeetCode 1번] Two Sum/안준홍_브루트 포스 풀이.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        # 이중 for문을 통해 두 수의 합을 구하여 target과 비교
         for idx1 in range(len(nums)):
             for idx2 in range(idx1+1, len(nums)):
                 if nums[idx1] + nums[idx2] == target:
