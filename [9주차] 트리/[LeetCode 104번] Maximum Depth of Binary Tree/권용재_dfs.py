c8b09b39deb494f0e9c34d8f979cd925901663fb
권용재 <yongjae116@gmail.com>
2022-09-24 18:41:17 +0900

    뺴먹은거 추가_권용재

---
+++ [9주차] 트리/[LeetCode 104번] Maximum Depth of Binary Tree/권용재_dfs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
