795770be25f975f96cee8c4f6cf3e0b2ec0dfbc2
yudavid <loveneverfails95@naver.com>
2022-10-08 21:32:00 +0900

    1008

--- [10주차] 힙, 트라이/[LeetCode 208번] Implement Trie(Prefix Tree)/유다윗_class구현.py
+++ [10주차] 힙, 트라이/[LeetCode 208번] Implement Trie(Prefix Tree)/유다윗_class구현.py
@@ -1,50 +1,93 @@
 # 208. Implement Trie (Prefix Tree)
-# 14번째 tc에서 막힘
 
+# 결국 교재... 쓰읍
 from collections import defaultdict
 
-class Trie:
-    tries = defaultdict()                       # key: 루트 키값(word의 첫 글자) / value: 해당 루트의 trie와 final
-                                                # final: 각 단어의 끝나는 글자와 그 글자의 인덱스를 넣어둘 리스트
+class Tree:
     def __init__(self):
-        Trie.tries = defaultdict()              # tries 초기화
+        self.ch = defaultdict(Tree)
+        self.check = False
+
+class Trie:
+    def __init__(self):
+        self.trie = Tree()              # tries 초기화
         
     def insert(self, word: str):
-        if word[0] not in Trie.tries:           # 새로운 알파벳으로 시작하는 단어일 경우 tries에 초기값 할당
-            Trie.tries[word[0]] = [defaultdict(set), list()]
-        
-        trie = Trie.tries[word[0]][0]
-        final = Trie.tries[word[0]][1]
-        p = -1
-        for i in range(len(word)):              # 각 레벨마다 글자 추가
-            trie[i].add((word[i], p))
-            p = word[i]
-        final.append([word[-1], len(word)-1])
+        node = self.trie
+        for w in word:
+            node = node.ch[w]
+        node.check = True
 
     def search(self, word: str) -> bool:
-        if word[0] not in Trie.tries:
-            return False
-        trie, final = Trie.tries[word[0]]
-        
-        p = -1
-        for i in range(len(word)):
-            if (word[i], p) not in trie[i]:          # 글자가 없을 경우 False
+        node = self.trie
+        for w in word:
+            if w in node.ch:
+                node = node.ch[w]
+            else:
                 return False
-            p = word[i]
-        if [word[-1], len(word)-1] in final:    # final에 끝나는 글자와 인덱스가 있을 경우 단어가 있으므로 True
+        if node.check:
             return True
-        else:
-            return False
+        return False
+
     
     def startsWith(self, prefix: str) -> bool:
-        if prefix[0] not in Trie.tries:
-            return False
-        trie = Trie.tries[prefix[0]][0]
-
-        p = -1
-        for i in range(len(prefix)):
-            if (prefix[i], p) not in trie[i]:
+        node = self.trie
+        for w in prefix:
+            if w in node.ch:
+                node = node.ch[w]
+            else:
                 return False
-            p = prefix[i]
-        else:
-            return True                         # fianl은 따로 확인할 필요 없음
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######################################################
+
+# 14번째 tc에서 막힘
+class Tree:
+    def __init__(self):
+        self.ch = defaultdict(Tree)
+        self.check = False
+
+class Trie:
+    def __init__(self):
+        self.trie = Tree()              # tries 초기화
+        
+    def insert(self, word: str):
+        node = self.trie
+        for w in word:
+            node = node.ch[w]
+        node.check = True
+
+    def search(self, word: str) -> bool:
+        node = self.trie
+        for w in word[:-1]:
+            if len(node.ch[w].keys()):
+                node = node.ch[w]
+            else:
+                return False
+        if node.check:
+            return True
+        return False
+
+    
+    def startsWith(self, prefix: str) -> bool:
+        node = self.trie
+        for w in prefix:
+            if len(node.ch[w].keys()):
+                node = node.ch[w]
+            else:
+                return False
+        return True
