a7e169357f70de86f9195ff609c97983f40d5ad9
Junhong Ahn <junhong625@naver.com>
2022-10-28 11:24:49 +0900

    1028 안준홍

--- [10주차] 힙, 트라이/[LeetCode 208번] Implement Trie(Prefix Tree)/안준홍_dict활용.py
+++ [10주차] 힙, 트라이/[LeetCode 208번] Implement Trie(Prefix Tree)/안준홍_dict활용.py
@@ -1,29 +1,29 @@
 class Trie:
 
-    def __init__(self, data=None):
-        self.trie = {}
-        self.data = data
+    def __init__(self):
+        self.trie = {}                              # dictionary로 trie 구현
+        self.end = False                            # 단어의 끝 지점을 표시하는 기준이 될 변수 
         
     def insert(self, word: str) -> None:
-        std = self
-        for char in word:
-            if char not in std.trie:
-                std.trie[char] = Trie()
-            std = std.trie[char]
-        std.data = word
+        std = self                                  # std를 기준으로 설정
+        for char in word:                           # 단어에서 문자를 하나씩 순회
+            if char not in std.trie:                # 해당 문자가 trie에 없을 경우
+                std.trie[char] = Trie()             # trie에 Key를 문자로, value를 Trie 클래스로 설정한 dict 생성
+            std = std.trie[char]                    # 해당 문자로 std(기준점) 이동
+        std.end = True                              # 순회가 끝난 후 마지막 기준점에 단어의 끝 지점이라는 의미로 True로 변경 
             
     def search(self, word: str) -> bool:
-        std = self
-        for char in word:
-            if char not in std.trie:
-                return False
-            std = std.trie[char]
-        return True if std.data == word else False
+        std = self                                  # std를 기준으로 설정
+        for char in word:                           # 단어에서 문자를 하나씩 순회
+            if char not in std.trie:                # 해당 문자가 trie에 없을 경우
+                return False                        # False를 반환
+            std = std.trie[char]                    # 해당 문자로 std(기준점) 이동
+        return std.end                              # 순회가 끝난 후 마지막 기준점에 존재하는 end로 해당 단어가 존재하는지 아닌지 판단
         
     def startsWith(self, prefix: str) -> bool:
-        std = self
-        for char in prefix:
-            if char not in std.trie:
-                return False
-            std = std.trie[char]
-        return True
+        std = self                                  # std를 기준으로 설정
+        for char in prefix:                         # prefix에서 문자를 하나씩 순회
+            if char not in std.trie:                # 문자가 trie에 없을 경우 
+                return False                        # 접두사에 해당되지 않기에 False를 반환
+            std = std.trie[char]                    # 해당 문자로 std(기준점) 이동
+        return True                                 # 순회를 무사히 마쳤다면 접두사이기에 True를 반환
